d576265df7fb16a501ed3ce9cbb280512fbfb551
Jorim <j.vanderwal@students.uu.nl>
2021-07-09 22:24:15 +0200

    Fix gun shooting in weird directions

--- TrainGame/Assets/Scripts/Revolver.cs
+++ TrainGame/Assets/Scripts/Revolver.cs
@@ -9,10 +9,9 @@ public class Revolver : Gun
         Bullet bullet = Instantiate(BulletPrefab, transform.position + ShootFromPosition, Quaternion.identity).GetComponent<Bullet>();
         bullet.BulletSpeed = BulletSpeed;
 
-        Vector3 mousePlayerDiff = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+        Vector3 mousePlayerDiff = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
         Debug.Log(mousePlayerDiff);
         Debug.DrawRay(transform.position, mousePlayerDiff * 10, Color.green, 2);
-        Debug.DrawRay(transform.position, new Vector3(mousePlayerDiff.x, 0, mousePlayerDiff.y) * 10, Color.red,2 );
-        bullet.Direction = new Vector3(mousePlayerDiff.x, 0, mousePlayerDiff.y);
+        bullet.Direction = new Vector3(mousePlayerDiff.x, mousePlayerDiff.y);
     }
 }
