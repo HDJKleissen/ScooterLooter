54b293504109165b3fe23ed48ab33df4d90eb884
Jorim <jorimvanderwal@gmail.com>
2021-07-09 22:46:04 +0200

    Change coroutine construction to built in functionality

--- TrainGame/Assets/Scripts/Bullet.cs
+++ TrainGame/Assets/Scripts/Bullet.cs
@@ -9,10 +9,12 @@ public class Bullet : MonoBehaviour
     public float BulletSpeed;
     public ActorController Owner;
 
+    const float secondsAliveUntilDestroyed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CoroutineHelper.DelaySeconds(() => Destroy(gameObject), 5));
+        Destroy(gameObject, secondsAliveUntilDestroyed);
     }
 
     // Update is called once per frame
