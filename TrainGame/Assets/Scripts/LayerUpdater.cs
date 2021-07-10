faa3b4451c11a600c557a66854b0220c3727c720
Jorim <jorimvanderwal@gmail.com>
2021-07-10 11:39:45 +0200

    Add script to automatically sort sprites in a layer based on position

---
+++ TrainGame/Assets/Scripts/LayerUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class LayerUpdater : MonoBehaviour
+{
+    [SerializeField]
+    bool StaticObject;
+
+    SpriteRenderer renderer;
+    Collider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+
+        if (collider != null)
+            renderer.sortingOrder = -(int)(collider.bounds.center.y * 10);
+        else
+            renderer.sortingOrder = -(int)(transform.position.y * 10);
+        if (StaticObject)
+            Destroy(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collider != null)
+            renderer.sortingOrder = -(int)(collider.bounds.center.y * 10);
+        else
+            renderer.sortingOrder = -(int)(transform.position.y * 10);
+    }
+}
