5f688eb690137546354a360977703d49b9f13e23
Yannick Meijer <meyannick@hotmail.com>
2021-07-11 16:08:56 +0200

    Added spawning carts

--- TrainGame/Assets/Scripts/TrainController.cs
+++ TrainGame/Assets/Scripts/TrainController.cs
@@ -10,15 +10,35 @@ public class TrainController : ActorController
         }
     }
 
+    public bool SlownDownTrain;
+
     bool slowingDown = false;
     protected override Vector3 GetMovement()
     {
+        if (SlownDownTrain)
+        {
+            return SlowingDownMovement();
+        }
+        else
+        {
+            return NormalMovement();
+        }
+    }
+
+    private Vector3 NormalMovement()
+    {
+        return new Vector3(-1, 0, 0);
+    }
+
+    private Vector3 SlowingDownMovement()
+    {
+
         Vector3 nulledPosition = transform.position;
         nulledPosition.y = 0;
 
         if (!slowingDown)
         {
-            if (Vector3.Distance(nulledPosition, new Vector3(10,0,0)) > 0.5f)
+            if (Vector3.Distance(nulledPosition, new Vector3(10, 0, 0)) > 0.5f)
             {
                 return new Vector3(-1, 0, 0);
             }
