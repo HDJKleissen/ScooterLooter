19398c265fe8b58f6ea7d26a98072a4e4f6d66ca
Jorim <jorimvanderwal@gmail.com>
2021-08-22 15:42:09 +0200

    Add utility function to Lists

---
+++ TrainGame/Assets/Scripts/Util/ListExtension.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtension
+{
+    public static T GetRandom<T>(this List<T> list)
+    {
+        return list[Random.Range(0, Random.Range(0, list.Count))];
+    }
+}
